132fcbb02cc4b2d34659503aa999dd214c12217d
iahuang <iahuang@users.noreply.github.com>
2020-12-21 15:48:29 -0500

    cleaning up the repository

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from core import assembler
 
 asm = assembler.Assembly()
-asm.loadSourceFile("test.s")
+asm.loadSourceFile("test/test.s")
 asm.assemble()
-asm.exportAsBinary("test.bin")
-asm.exportDebugFile("test.debug")
+asm.exportAsBinary("test/test.bin")
+asm.exportDebugFile("test/test.debug")
